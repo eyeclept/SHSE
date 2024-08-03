a9c9d931426f1ef12226034e32bdeffa82472ad3
eyeclept <eyeclept@pm.me>
2024-08-03 13:05:48 -0400

    did some formatting to match pep8

--- search/web.py
+++ search/web.py
@@ -1,24 +1,17 @@
 """
-Author:  Richard Baldwin
-Date:    /2024
-E-mail:  eyeclept@pm.me
-Description: 
-    -
-      
+Author: Richard Baldwin
+Date:   2024
+Email: eyeclept@pm.me
+
+Description:
+
 """
-# imports
-
-# constants
-
-# classes
-
-# functions
 def function():
     """
-    use-----> 
-    input---> 
-    output--> 
-    details-> 
-    
+    Input: None
+    Output: None
+    Details:
+        
     """
     pass
+
